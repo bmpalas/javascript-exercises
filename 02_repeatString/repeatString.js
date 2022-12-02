4b47cd60edfbbf1a797b0b6714a8dac4ac3408ab
Brandon Palas <bmpalas393@gmail.com>
2022-12-01 19:57:12 -0800

    complete exercises 01-03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(input, times) {
+    let accumulator = input;
+    if (times === 0) {return ''}
+    else if (times < 0 ) {return('ERROR')}
+    else {for (i = 1; i < times; i++) {
+          accumulator = accumulator + input;
+          }
+    }
+        return accumulator;
+    //that's right, a function is all about what it retuns
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    array = [] 
+    const strLenth = input.length;
+    for (i = 0; i < strLenth; i++) {
+        let char = input.charAt(i);
+        array.push(char);
+    }
+    let accumulator = ''
+    for (i = (array.length); i > 0; i-- ) {
+        let arrayChar = array.pop();
+        accumulator += arrayChar;
+    }
+    return accumulator;
 
+  
+
+
+    /* 
+put string into an array
+reverse the array
+output the array as a string -- pop each 
+
+*/
 };
 
 // Do not edit below this line
