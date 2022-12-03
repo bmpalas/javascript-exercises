50e7952e012c64951b86f34b391fbc01db05e8fd
Brandon Palas <bmpalas393@gmail.com>
2022-12-02 18:47:09 -0800

    complete testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num1 < 0 | num2 < 0) {
+        return 'ERROR';
+    } 
+    else if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return 'ERROR';
+    }    // first, return ERROR if negative or non-number
+    if (num1 >= num2) {
+       bigNum = num1;
+       smallNum = num2;
+    } else {
+       bigNum = num2;
+       smallNum = num1; 
+    } // figure out which number is bigger, and assign variables
+    let accumulator = smallNum;
+    for (i = smallNum ; i <= bigNum; ++i) {
+        accumulator += i;
+    }
+   
+    return (accumulator - 1);
 };
 
 // Do not edit below this line
