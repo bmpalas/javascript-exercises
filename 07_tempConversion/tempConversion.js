cdeaf0d4435f89eff0846bae54ad4be80e17783d
Brandon Palas <bmpalas393@gmail.com>
2022-12-02 19:27:12 -0800

    first test success

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(input) {
+  calculation = ((input - 32) * (5/9));
+  result = Math.round(calculation);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function() {  
+  calculation = (input * (9/5) + 32);
+  result = Math.round(calculation);
+  return result;
 };
 
 // Do not edit below this line
