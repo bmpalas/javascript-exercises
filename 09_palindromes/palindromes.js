50502ca3101946446d8325222cc03d98aa46a413
Brandon Palas <bmpalas939@gmail.com>
2022-12-04 08:31:02 -0800

    complete first two tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (input) {
+const punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g; //regex of all punctuation
+const noPunc = input.replace(punc, ''); //strip punctuation
+const stripStr = noPunc; // creat stripped string
+const arr = stripStr.split('');
+const arrRev = arr.reverse();
+const strRev = arrRev.join('');
 
+if (strRev == stripStr) {
+    return true;
+} else {
+    return false;
+}
 };
 
 // Do not edit below this line
