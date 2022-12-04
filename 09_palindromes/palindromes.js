f663a7e9381a01053234eb4059330326a53d7e37
Brandon Palas <bmpalas939@gmail.com>
2022-12-04 08:32:40 -0800

    complete third test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,8 @@
 const palindromes = function (input) {
 const punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g; //regex of all punctuation
 const noPunc = input.replace(punc, ''); //strip punctuation
-const stripStr = noPunc; // creat stripped string
+const noCap = noPunc.toLowerCase();
+const stripStr = noCap; // creat stripped string
 const arr = stripStr.split('');
 const arrRev = arr.reverse();
 const strRev = arrRev.join('');
