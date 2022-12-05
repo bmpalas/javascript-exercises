42203262b884e97a300223859755ee4309357c45
Brandon Palas <bmpalas939@gmail.com>
2022-12-04 17:46:09 -0800

    pass initial tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    arrFib = [0,1];
+    for (let i = 0; i <= num; i++) {
+        let index1 = -2
+        let index2 = -1
+        arrFib.push((arrFib.at(index1) + arrFib.at(index2)));
+    }
+  return arrFib.at(num);
 };
-
+/* 
+loop only needs to go as high as num
+for each iteration, add i-1 and i-2? and push to array? or just get the final result // array is better
+*/
 // Do not edit below this line
 module.exports = fibonacci;
