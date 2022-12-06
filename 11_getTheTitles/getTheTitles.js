a8c07f43ee6b6d94bfcfb1cacaca3f0c847c0729
Brandon Palas <bmpalas939@gmail.com>
2022-12-06 11:17:59 -0800

    finally complete the test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,22 @@
 const getTheTitles = function() {
+        const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
 
+
+const a = []; // creat the array
+books.forEach(function(obj){ //use Array.forEach()
+    a.push(obj.title); // for each element, use Array.push() to push the 
+    //element.title) 
+})
+return a;
 };
 
 // Do not edit below this line
