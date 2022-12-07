f8837803d12dc18edee5e1b7cb7f32d59d0c68b5
Brandon Palas <bmpalas939@gmail.com>
2022-12-06 16:48:07 -0800

    early attempts

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
 const findTheOldest = function() {
 
+const a = []; // creat the array
+people.forEach(function(obj){ //use Array.forEach()
+    a.push(obj.yearOfBirth); // for each element, use Array.push() to push the 
+    //element.title) 
+})
+return a;
 };
 
 // Do not edit below this line
